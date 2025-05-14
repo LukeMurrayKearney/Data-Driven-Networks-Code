06da33f22bc5207bb40f19bf2aff437bf13b81e3
LukeMurrayKearney <u2288335@live.warwick.ac.uk>
2025-05-14 12:36:21 +0100

    more sims

--- _final_sellke_scale_gmm.py
+++ _final_sellke_scale_gmm.py
@@ -57,6 +57,6 @@ for i, data in enumerate(datas):
             
             result = nd_p.gmm_sims(samples,partitions=partitions,taus=taus[i], iterations=iters, inv_gamma=7, prop_infec=10/n, scaling=scales[j])
                 
-            with open(f'output_data/gmm/1_{k+24}_{data}_{model}_scale.json','w') as f:
+            with open(f'output_data/gmm/1_{k+29}_{data}_{model}_scale.json','w') as f:
                 json.dump(result, f)
 print('done')
