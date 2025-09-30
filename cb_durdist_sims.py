fcb166a8bfbe9857e234975f98c9b2bb2c4a9c37
LukeMurrayKearney <u2288335@live.warwick.ac.uk>
2025-09-30 09:40:05 +0100

    more sims with a higher rate

--- cb_durdist_sims.py
+++ cb_durdist_sims.py
@@ -10,7 +10,7 @@ import math
 n = 100_000
 num_networks= 20
 
-taus = np.arange(0.05,2.05,0.05)
+taus = np.arange(2.05,5.05,0.05)
 
 buckets = np.array([5,12,18,30,40,50,60,70])
 partitions = [0.058*n, 0.145*n, 0.212*n, 0.364*n, 0.497*n, 0.623*n, 0.759*n, 0.866*n, n]
@@ -47,5 +47,5 @@ for i, data in enumerate(datas):
                 samples.append([int(np.round(np.exp(b)-1)) if int(np.round(np.exp(b)-1))>=0 else 0 for b in sample])
                 samples_for_plot[-1].append([int(np.round(np.exp(b)-1)) if int(np.round(np.exp(b)-1))>=0 else 0 for b in sample])
         res = nd_p.gmm_dur(samples,partitions=partitions,num_dur=3, taus=taus, iterations=48,props=props.tolist())
-        with open(f'duration+ages/sims/{data}_{k}.json','w') as f:
+        with open(f'duration+ages/sims/{data}_{k+20}.json','w') as f:
             json.dump(res, f)
