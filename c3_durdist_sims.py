33f003744c1be8312113f0c224c4f56030f23382
LukeMurrayKearney <u2288335@live.warwick.ac.uk>
2025-10-11 16:38:32 +0100

    new sims duration

--- c3_durdist_sims.py
+++ c3_durdist_sims.py
@@ -10,7 +10,9 @@ import math
 n = 100_000
 num_networks= 20
 
-taus = np.arange(2.05,5.05,0.05)
+t1 = np.arange(.05,1.05,0.05)
+t2 = np.arange(1.1,6.1,.333)
+taus = np.concatenate((t1,t2))
 
 buckets = np.array([5,12,18,30,40,50,60,70])
 partitions = [0.058*n, 0.145*n, 0.212*n, 0.364*n, 0.497*n, 0.623*n, 0.759*n, 0.866*n, n]
