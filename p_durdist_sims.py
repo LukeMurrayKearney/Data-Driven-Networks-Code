7a16d259fc452d3bdfc92e70837f0382bc5b06f9
LukeMurrayKearney <u2288335@live.warwick.ac.uk>
2025-11-06 15:55:09 +0000

    more sims

--- p_durdist_sims.py
+++ p_durdist_sims.py
@@ -10,7 +10,7 @@ import math
 n = 100_000
 num_networks= 20
 
-taus = np.arange(.015, .0315, 0.01)
+taus = np.arange(.01, .035, 0.01)
 
 
 buckets = np.array([5,12,18,30,40,50,60,70])
@@ -48,5 +48,5 @@ for i, data in enumerate(datas):
                 samples.append([int(np.round(np.exp(b)-1)) if int(np.round(np.exp(b)-1))>=0 else 0 for b in sample])
                 samples_for_plot[-1].append([int(np.round(np.exp(b)-1)) if int(np.round(np.exp(b)-1))>=0 else 0 for b in sample])
         res = nd_p.gmm_dur_gillesp(samples,partitions=partitions,num_dur=3, taus=taus, iterations=48,props=props.tolist(),num_infec=5)
-        with open(f'duration+ages/seir_sims/{data}_{k+40}.json','w') as f:
+        with open(f'duration+ages/seir_sims/{data}_{k+60}.json','w') as f:
             json.dump(res, f)
