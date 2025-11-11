314f7cb4e7a598a0ac636f8ac947bbf20e0dfeba
LukeMurrayKearney <u2288335@live.warwick.ac.uk>
2025-11-11 15:59:16 +0000

    better params sims

--- cb_sims.py
+++ cb_sims.py
@@ -7,7 +7,8 @@ import math
 n = 100_000
 num_networks= 20
 
-taus = np.arange(.01, .31, .01)
+taus1 = np.arange(.001, .041, .001)
+taus2 = np.arange(.05, 10.05, .1)
 
 buckets = np.array([5,12,18,30,40,50,60,70])
 partitions = [0.058*n, 0.145*n, 0.212*n, 0.364*n, 0.497*n, 0.623*n, 0.759*n, 0.866*n, n]
@@ -43,10 +44,10 @@ for i, data in enumerate(datas):
             for sample in samples_tmp:
                 samples.append([int(np.round(np.exp(b)-1)) if int(np.round(np.exp(b)-1))>=0 else 0 for b in sample])
                 samples_for_plot[-1].append([int(np.round(np.exp(b)-1)) if int(np.round(np.exp(b)-1))>=0 else 0 for b in sample])
-        res = nd_p.gmm_gillesp(samples,partitions=partitions,taus=taus, iterations=48, num_infec=5)
+        res = nd_p.gmm_gillesp(samples,partitions=partitions,taus=taus1, iterations=48, num_infec=5)
         with open(f'duration+ages/seir_sims/{data}_{k}_nodur.json','w') as f:
             json.dump(res, f)
         
-        res = nd_p.sbm_gillesp(contact_matrix=cm, partitions=partitions, taus=taus, iterations=48, num_infec=5)
+        res = nd_p.sbm_gillesp(contact_matrix=cm, partitions=partitions, taus=taus2, iterations=48, num_infec=5)
         with open(f'duration+ages/seir_sims/{data}_{k}_sbm.json','w') as f:
             json.dump(res, f)
