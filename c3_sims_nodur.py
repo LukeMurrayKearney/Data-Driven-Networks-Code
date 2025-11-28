aa474cf11bd7c81aada77ec8f2ef12afbd1fd197
LukeMurrayKearney <u2288335@live.warwick.ac.uk>
2025-11-28 17:43:26 +0000

    more gen1 sims

--- c3_sims_nodur.py
+++ c3_sims_nodur.py
@@ -10,7 +10,7 @@ import math
 n = 100_000
 num_networks= 20
 
-taus1 = np.arange(.0001, .0051, .0001)
+taus1 = np.arange(.00005, .00175, .0001)
 taus2 = np.arange(.00001, .0001, .00001)
 
 buckets = np.array([5,12,18,30,40,50,60,70])
@@ -47,10 +47,10 @@ for i, data in enumerate(datas):
             for sample in samples_tmp:
                 samples.append([int(np.round(np.exp(b)-1)) if int(np.round(np.exp(b)-1))>=0 else 0 for b in sample])
                 samples_for_plot[-1].append([int(np.round(np.exp(b)-1)) if int(np.round(np.exp(b)-1))>=0 else 0 for b in sample])
-        res = nd_p.gmm_gillesp(samples,partitions=partitions,taus=taus1, iterations=48, num_infec=5)
-        with open(f'duration+ages/seir_sims/{data}_{k+160}_nodur.json','w') as f:
+        res = nd_p.gmm_gillesp(samples,partitions=partitions,taus=taus1, iterations=48, num_infec=1)
+        with open(f'duration+ages/seir_sims/{data}_{k}_nodur_gen1.json','w') as f:
             json.dump(res, f)
-        res = nd_p.gmm_gillesp(samples,partitions=partitions,taus=taus2, iterations=48, num_infec=5)
-        with open(f'duration+ages/seir_sims/{data}_{k+180}_nodur.json','w') as f:
+        res = nd_p.gmm_gillesp(samples,partitions=partitions,taus=taus2, iterations=48, num_infec=1)
+        with open(f'duration+ages/seir_sims/{data}_{k+20}_nodur_gen1.json','w') as f:
             json.dump(res, f)
 
--- c3_sims_sbm.py
+++ c3_sims_sbm.py
@@ -10,7 +10,8 @@ import math
 n = 100_000
 num_networks= 20
 
-taus2 = np.arange(.0025, .1, .0025)
+taus1 = np.arange(.001,0.1,0.02)
+taus2 = np.arange(.1, .5, .01)
 
 buckets = np.array([5,12,18,30,40,50,60,70])
 partitions = [0.058*n, 0.145*n, 0.212*n, 0.364*n, 0.497*n, 0.623*n, 0.759*n, 0.866*n, n]
@@ -31,6 +32,6 @@ for i, data in enumerate(datas):
     cm = np.genfromtxt(f'input_data/contact_matrices/contact_matrix_{data}.csv', delimiter=',')
 
     for k in range(num_networks):
-        res = nd_p.sbm_gillesp(contact_matrix=cm, partitions=partitions, taus=taus2, iterations=48, num_infec=5)
-        with open(f'duration+ages/seir_sims/{data}_{k+140}_sbm.json','w') as f:
+        res = nd_p.sbm_gillesp(contact_matrix=cm, partitions=partitions, taus=taus2, iterations=48, num_infec=1)
+        with open(f'duration+ages/seir_sims/{data}_{k}_sbm_gen1.json','w') as f:
             json.dump(res, f)
