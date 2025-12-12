0e4be3bbe1369d2cc358b25e941ea9d11defd95d
LukeMurrayKearney <u2288335@live.warwick.ac.uk>
2025-12-12 00:49:25 +0000

    some of 80+

--- c3_sims_nodur.py
+++ c3_sims_nodur.py
@@ -49,6 +49,6 @@ for i, data in enumerate(datas):
                 samples.append([int(np.round(np.exp(b)-1)) if int(np.round(np.exp(b)-1))>=0 else 0 for b in sample])
                 samples_for_plot[-1].append([int(np.round(np.exp(b)-1)) if int(np.round(np.exp(b)-1))>=0 else 0 for b in sample])
         res = nd_p.gmm_gillesp(samples,partitions=partitions,taus=taus, iterations=48, num_infec=1)
-        with open(f'duration+ages/seir_sims/{data}_{k+60}_nodur_fin.json','w') as f:
+        with open(f'duration+ages/seir_sims/{data}_{k+80}_nodur_fin.json','w') as f:
             json.dump(res, f)
 
