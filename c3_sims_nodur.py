a73656a0495a439df7ff2bcfe3c7555ac59d4503
LukeMurrayKearney <u2288335@live.warwick.ac.uk>
2025-12-12 13:46:21 +0000

    c3 120+

--- c3_sims_nodur.py
+++ c3_sims_nodur.py
@@ -10,8 +10,8 @@ import math
 n = 100_000
 num_networks= 20
 
-taus1 = np.arange(.000075, .00175, .00025)
-taus2 = np.arange(.00002, .000075, .000025)
+taus1 = np.arange(.0018, .01, .0002)
+taus2 = np.arange(.01, .1, 0.05)
 taus = np.concatenate((taus1, taus2))
 
 buckets = np.array([5,12,18,30,40,50,60,70])
@@ -49,6 +49,6 @@ for i, data in enumerate(datas):
                 samples.append([int(np.round(np.exp(b)-1)) if int(np.round(np.exp(b)-1))>=0 else 0 for b in sample])
                 samples_for_plot[-1].append([int(np.round(np.exp(b)-1)) if int(np.round(np.exp(b)-1))>=0 else 0 for b in sample])
         res = nd_p.gmm_gillesp(samples,partitions=partitions,taus=taus, iterations=48, num_infec=1)
-        with open(f'duration+ages/seir_sims/{data}_{k+100}_nodur_fin.json','w') as f:
+        with open(f'duration+ages/seir_sims/{data}_{k+120}_nodur_fin.json','w') as f:
             json.dump(res, f)
 
