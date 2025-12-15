b4911d60d8a8615633d1db3b6ea352fcdc66366a
LukeMurrayKearney <u2288335@live.warwick.ac.uk>
2025-12-15 18:12:47 +0000

    more duration dist sims

--- ca_durdist_sims.py
+++ ca_durdist_sims.py
@@ -12,7 +12,8 @@ num_networks= 20
 
 taus1 = np.arange(0.05, 4.05, 0.1)
 taus2 = np.arange(4, 5.5, .25)
-taus3 = np.arange(5.5, 10, 1)
+taus3 = np.arange(5.5, 10, .5)
+taus4 = np.arange(10, 35, 1)
 taus = np.concatenate((taus1, taus2, taus3))
 
 
@@ -51,5 +52,5 @@ for i, data in enumerate(datas):
                 samples.append([int(np.round(np.exp(b)-1)) if int(np.round(np.exp(b)-1))>=0 else 0 for b in sample])
                 samples_for_plot[-1].append([int(np.round(np.exp(b)-1)) if int(np.round(np.exp(b)-1))>=0 else 0 for b in sample])
         res = nd_p.gmm_dur_gillesp(samples,partitions=partitions,num_dur=3, taus=taus, iterations=48,props=props.tolist(), num_infec=1)
-        with open(f'duration+ages/seir_sims/{data}_{k+140}_fin.json','w') as f:
+        with open(f'duration+ages/seir_sims/{data}_{k+160}_fin.json','w') as f:
             json.dump(res, f)
--- cb_durdist_sims.py
+++ cb_durdist_sims.py
@@ -13,7 +13,8 @@ num_networks= 20
 taus1 = np.arange(.02, .55, .02)
 taus2 = np.arange(.55, 2.25, .1)
 taus3 = np.arange(2.25, 12.5, .5)
-taus = np.concatenate((taus1, taus2, taus3))
+taus4 = np.arange(12.5, 35.5, 1)
+taus = np.concatenate((taus1, taus2, taus3, taus4))
 
 
 buckets = np.array([5,12,18,30,40,50,60,70])
@@ -51,5 +52,5 @@ for i, data in enumerate(datas):
                 samples.append([int(np.round(np.exp(b)-1)) if int(np.round(np.exp(b)-1))>=0 else 0 for b in sample])
                 samples_for_plot[-1].append([int(np.round(np.exp(b)-1)) if int(np.round(np.exp(b)-1))>=0 else 0 for b in sample])
         res = nd_p.gmm_dur_gillesp(samples,partitions=partitions,num_dur=3, taus=taus, iterations=48,props=props.tolist(),num_infec=1)
-        with open(f'duration+ages/seir_sims/{data}_{k+120}_fin.json','w') as f:
+        with open(f'duration+ages/seir_sims/{data}_{k+140}_fin.json','w') as f:
             json.dump(res, f)
