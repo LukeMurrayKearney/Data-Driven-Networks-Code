7990ee0097e17cddc8f309c64b13d46f927cfe5b
LukeMurrayKearney <u2288335@live.warwick.ac.uk>
2025-12-15 12:01:24 +0000

    60+ dur noage

--- c3_durdist_sims_noage.py
+++ c3_durdist_sims_noage.py
@@ -10,8 +10,8 @@ import math
 n = 100_000
 num_networks= 20
 
-taus1 = np.arange(.02, 1.02, .02)
-taus2 = np.arange(1.25, 10, .5)
+taus1 = np.arange(.03, 1, .02)
+taus2 = np.arange(1, 10, .5)
 taus = np.concatenate((taus1, taus2))
 
 buckets = np.array([])
@@ -49,5 +49,5 @@ for i, data in enumerate(datas):
                 samples.append([int(np.round(np.exp(b)-1)) if int(np.round(np.exp(b)-1))>=0 else 0 for b in sample])
                 samples_for_plot[-1].append([int(np.round(np.exp(b)-1)) if int(np.round(np.exp(b)-1))>=0 else 0 for b in sample])
         res = nd_p.gmm_dur_gillesp(samples,partitions=partitions,num_dur=3, taus=taus, iterations=48,props=props.tolist(), num_infec=1)
-        with open(f'duration+ages/seir_sims/{data}_{k+40}_noage_fin.json','w') as f:
+        with open(f'duration+ages/seir_sims/{data}_{k+60}_noage_fin.json','w') as f:
             json.dump(res, f)
