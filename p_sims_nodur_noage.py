9fd94cc365fe9234f9aaeb7b0e433b5d8681f260
LukeMurrayKearney <u2288335@live.warwick.ac.uk>
2026-01-16 00:23:48 +0000

    more poly

--- p_sims_nodur_noage.py
+++ p_sims_nodur_noage.py
@@ -5,7 +5,7 @@ import sklearn.mixture
 import math
 
 n = 100_000
-num_networks= 50
+num_networks= 40
 
 # taus2 = np.arange(.00025, .006, .0005)
 # taus1 = np.arange(.006, .055, .002)
@@ -48,5 +48,5 @@ for i, data in enumerate(datas):
                 samples.append([int(np.round(np.exp(b)-1)) if int(np.round(np.exp(b)-1))>=0 else 0 for b in sample])
                 samples_for_plot[-1].append([int(np.round(np.exp(b)-1)) if int(np.round(np.exp(b)-1))>=0 else 0 for b in sample])
         res = nd_p.gmm_gillesp(samples,partitions=partitions,taus=taus, iterations=48, num_infec=1)
-        with open(f'duration+ages/seir_sims/{data}_{k+160}_nodur_noage_fin.json','w') as f:
+        with open(f'duration+ages/seir_sims/{data}_{k+210}_nodur_noage_fin.json','w') as f:
             json.dump(res, f)
