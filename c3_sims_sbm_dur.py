cc896c1c1de0ea5179f65c07c439d4d8472fa2b6
LukeMurrayKearney <u2288335@live.warwick.ac.uk>
2026-01-25 23:13:43 +0000

    sbm dur 40+

--- c3_sims_sbm_dur.py
+++ c3_sims_sbm_dur.py
@@ -10,9 +10,9 @@ import math
 n = 100_000
 num_networks= 40
 
-taus1 = np.arange(.45,0.8,0.05)
-taus2 = np.arange(2.5, 4.5, .25)
-taus3 = np.arange(10, 12, .5)
+taus1 = np.arange(.45,0.8,0.075)
+taus2 = np.arange(2.5, 4.5, .5)
+taus3 = np.arange(10, 12, 1)
 taus = np.concatenate((taus1, taus2, taus3))
 
 buckets = np.array([5,12,18,30,40,50,60,70])
@@ -47,5 +47,5 @@ for i, data in enumerate(datas):
     
     for k in range(num_networks):
         res = nd_p.sbm_gillesp_dur(contact_matrix=contact_matrix, num_dur=3, partitions=partitions, taus=taus, iterations=48, props=props.tolist(), num_infec=1)
-        with open(f'duration+ages/seir_sims/{data}_{k}_sbm_dur_fin.json','w') as f:
+        with open(f'duration+ages/seir_sims/{data}_{k+40}_sbm_dur_fin.json','w') as f:
             json.dump(res, f)
