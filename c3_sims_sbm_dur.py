95fde088e756aadc364d923e32c301259d74b072
LukeMurrayKearney <u2288335@live.warwick.ac.uk>
2026-01-26 16:43:27 +0000

    more sbm duration sims

--- c3_sims_sbm_dur.py
+++ c3_sims_sbm_dur.py
@@ -10,9 +10,9 @@ import math
 n = 100_000
 num_networks= 40
 
-taus1 = np.arange(.4,0.8,0.075)
-taus2 = np.arange(2.3, 4.5, .6)
-taus3 = np.arange(10.25, 12.25, 1)
+taus1 = np.arange(.35,0.8,0.075)
+taus2 = np.arange(2.6, 4.5, .6)
+taus3 = np.arange(10.75, 12.75, 1)
 taus = np.concatenate((taus1, taus2, taus3))
 
 buckets = np.array([5,12,18,30,40,50,60,70])
@@ -47,5 +47,5 @@ for i, data in enumerate(datas):
     
     for k in range(num_networks):
         res = nd_p.sbm_gillesp_dur(contact_matrix=contact_matrix, num_dur=3, partitions=partitions, taus=taus, iterations=48, props=props.tolist(), num_infec=1)
-        with open(f'duration+ages/seir_sims/{data}_{k+80}_sbm_dur_fin.json','w') as f:
+        with open(f'duration+ages/seir_sims/{data}_{k+120}_sbm_dur_fin.json','w') as f:
             json.dump(res, f)
--- p_sims_sbm_dur.py
+++ p_sims_sbm_dur.py
@@ -10,7 +10,7 @@ import math
 n = 100_000
 num_networks= 40
 
-taus = np.arange(.02,0.25,0.02)
+taus = np.arange(.015,0.25,0.02)
 
 buckets = np.array([5,12,18,30,40,50,60,70])
 partitions = [0.058*n, 0.145*n, 0.212*n, 0.364*n, 0.497*n, 0.623*n, 0.759*n, 0.866*n, n]
@@ -44,5 +44,5 @@ for i, data in enumerate(datas):
     
     for k in range(num_networks):
         res = nd_p.sbm_gillesp_dur(contact_matrix=contact_matrix, num_dur=3, partitions=partitions, taus=taus, iterations=48, props=props.tolist(), num_infec=1)
-        with open(f'duration+ages/seir_sims/{data}_{k+80}_sbm_dur_fin.json','w') as f:
+        with open(f'duration+ages/seir_sims/{data}_{k+120}_sbm_dur_fin.json','w') as f:
             json.dump(res, f)
