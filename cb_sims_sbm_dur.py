ce72acd88bfece0d5ea14946f4a79c6eb86f1a3c
LukeMurrayKearney <u2288335@live.warwick.ac.uk>
2026-02-01 23:49:43 +0000

    sbm dur 160+

--- cb_sims_sbm_dur.py
+++ cb_sims_sbm_dur.py
@@ -10,8 +10,8 @@ import math
 n = 100_000
 num_networks= 40
 
-taus1 = np.arange(.41,.76,0.025)
-taus2 = np.arange(8.25, 14.25, 1)
+taus1 = np.arange(.42,.78,0.025)
+taus2 = np.arange(7.5, 12.5, .5)
 taus = np.concatenate((taus1, taus2))
 # taus = np.array([.55,.575,.6,.626,.65,.675,.7,.725,.75,10,11,12,13,14,15,16,17,18])+.1
 
@@ -53,5 +53,5 @@ for i, data in enumerate(datas):
     for k in range(num_networks):
         print(f'network {k} for data {data}')
         res = nd_p.sbm_gillesp_dur(contact_matrix=contact_matrix, num_dur=3, partitions=partitions, taus=taus, iterations=48, props=props.tolist(), num_infec=1)
-        with open(f'duration+ages/seir_sims/{data}_{k+120}_{model}_fin.json','w') as f:
+        with open(f'duration+ages/seir_sims/{data}_{k+160}_{model}_fin.json','w') as f:
             json.dump(res, f)
